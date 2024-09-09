09e7352a580b3105e2a0eeaf8d56a0ebe8b60aeb
nivindulakshitha <nivindulakshitha@gmail.com>
2024-09-09 09:54:13 +0530

    Refactor App component to add HeroSection

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,11 @@
+import HeroSection from "./sections/HeroSection"
 import NavBar from "./sections/NavBar"
 
 const App = () => {
 	return (
 		<main className="max-w-7xl mx-auto">
 			<NavBar />
+			<HeroSection />
 		</main>
 	)
 }
---
+++ src/sections/HeroSection.jsx
@@ -0,0 +1,11 @@
+const HeroSection = () => {
+	return (
+		<section className="min-h-screen w-full flex flex-col relative">
+			<div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
+				<p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">Hi, I am Nivindu <span className="waving-hand">✋</span> </p>
+			</div>
+		</section>
+	)
+}
+
+export default HeroSection
