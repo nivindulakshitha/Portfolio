14226092362800b37c908a6466c16c80b602125a
nivindulakshitha <nivindulakshitha@gmail.com>
2024-09-09 10:11:22 +0530

    Refactor package dependencies and add React Three Fiber and Leva

--- src/sections/HeroSection.jsx
+++ src/sections/HeroSection.jsx
@@ -1,8 +1,20 @@
+import { PerspectiveCamera } from "@react-three/drei"
+import { Canvas } from "@react-three/fiber"
+
 const HeroSection = () => {
 	return (
 		<section className="min-h-screen w-full flex flex-col relative">
 			<div className="w-full mx-auto flex flex-col sm:mt-36 mt-20 c-space gap-3">
-				<p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">Hi, I am Nivindu <span className="waving-hand">✋</span> </p>
+				<p className="sm:text-3xl text-xl font-medium text-white text-center font-generalsans">
+					Hi, I am Nivindu <span className="waving-hand">✋</span>
+				</p>
+				<p className="hero_tag text-gray_gradient">Building Products & Brands</p>
+
+				<div className="w-full h-full absolute inset-0">
+					<Canvas className="w-full h-full">
+						<PerspectiveCamera makedefault position={[0, 0, 30]} />
+					</Canvas>
+				</div>
 			</div>
 		</section>
 	)
