80256f084418f0e5413c30fd33134bc6bb9266de
nivindulakshitha <nivindulakshitha@gmail.com>
2024-09-11 10:15:18 +0530

    Refactor Target component to add animation using gsap and @gsap/react

--- src/components/Target.jsx
+++ src/components/Target.jsx
@@ -1,11 +1,23 @@
 import { useGLTF } from "@react-three/drei"
 import { useRef } from "react"
+import gsap from 'gsap';
+import { useGSAP } from "@gsap/react";
 
 const Target = (props) => {
     const target = useRef();
     const { scene } = useGLTF('https://vazxmixjsiawhamofees.supabase.co/storage/v1/object/public/models/target-stand/model.gltf')
+
+    useGSAP(() => { 
+        gsap.to(target.current.position, {
+            y: target.current.position.y + 0.5, 
+            duration: 1.5,
+            repeat: -1,
+            yoyo: true
+        })
+    })
+
     return (
-        <mesh {...props} ref={target}>
+        <mesh {...props} ref={target} rotation={[0, Math.PI/5, 0]}>
             <primitive object={scene} />
         </mesh>
     )
