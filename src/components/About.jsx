3fd1d9c6a4f5f0873537109e4e97490b89bad017
nivindulakshitha <nivindulakshitha@gmail.com>
2024-09-26 20:25:47 +0530

    Refactor grid layout in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -17,8 +17,8 @@ const About = () => {
 
     return (
         <section className="c-space my-20">
-            <div className="grid xl:grid-cols-3 xl:grid-row-6 mg:grid-cols-2 grid-cols-1 gap-5 h-full">
-                <div className="col-sapn-1 xl:row-span-3">
+            <div className="grid xl:grid-cols-3 xl:grid-rows-3 md:grid-cols-2 grid-cols-1 gap-5 h-full">
+                <div className="col-span-1 xl:row-span-2">
                     <div className="grid-container">
                         <img src="/assets/grid1.png" alt="grid-1" className="w-full sm:h-[276px] h-fit object-contain" />
 
@@ -31,7 +31,7 @@ const About = () => {
                     </div>
                 </div>
 
-                <div className="col-span-1 xl:row-span-3">
+                <div className="col-span-1 xl:row-span-2">
                     <div className="grid-container">
                         <img src="/assets/grid2.png" alt="grid-2" className="w-full 2xl:w-[276px] h-fit object-contain" />
 
@@ -44,7 +44,7 @@ const About = () => {
                     </div>
                 </div>
 
-                <div className="col-span-1 2xl:row-span-3">
+                <div className="col-span-1 xl:row-span-3">
                     <div className="grid-container">
                         <div className="rounded-3xl w-full 2xl:h-[326px] h-fit flex justify-center items-center">
                             <Globe
@@ -69,16 +69,16 @@ const About = () => {
                         </div>
                     </div>
                 </div>
-                <div className="xl:col-span-2 xl:row-span-3">
+                <div className="xl:col-span-2 xl:row-span-2">
                     <div className="grid-container">
                         <img src="/assets/grid3.png" alt="grid-3" className="w-full sm:h-[266px] h-fit object-contain" />
                         <div>
                             <p className="grid-headtext">My passion for coding</p>
-                            <p className="grid-subtext">I love solving complex problems and creating beautiful and intuitive user experiences.</p>
+                            <p className="grid-subtext">I love solving complex problems and creating beautiful and intuitive user experiences. I am always looking for new and exciting ways to improve my skills. I am excited to continue learning and growing as a developer! </p>
                         </div>
                     </div>
                 </div>
-                <div className="xl:col-span-1 xl:row-span-2">
+                <div className="xl:col-span-1 xl:row-span-1">
                     <div className="grid-container">
                         <img src="assets/grid4.png" alt="grid-4" className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top" />
                         <div className="space-y-2">
@@ -87,7 +87,7 @@ const About = () => {
                                 <img src={hasCoped ? "assets/tick.svg" : "assets/copy.svg"} alt="copy" />
 
                                 <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">nivindulakshitha@gmial.com</p>
-                                
+
                             </div>
                         </div>
                     </div>
