32b9661bed47ac65cd5d6476d4d41fc7afae2778
nivindulakshitha <nivindulakshitha@gmail.com>
2024-09-26 20:06:29 +0530

    Add copy functionality to contact email in About component

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -1,7 +1,20 @@
 import Globe from "react-globe.gl"
 import Button from "./Button"
+import { useState } from "react"
 
 const About = () => {
+
+    const [hasCoped, setHasCoped] = useState(false)
+
+    const handleCopy = () => {
+        navigator.clipboard.writeText("nivindulakshitha@gmail.com")
+        setHasCoped(true)
+
+        setTimeout(() => {
+            setHasCoped(false)
+        }, 1000)
+    }
+
     return (
         <section className="c-space my-20">
             <div className="grid xl:grid-cols-3 xl:grid-row-6 mg:grid-cols-2 grid-cols-1 gap-5 h-full">
@@ -62,6 +75,20 @@ const About = () => {
                         <div>
                             <p className="grid-headtext">My passion for coding</p>
                             <p className="grid-subtext">I love solving complex problems and creating beautiful and intuitive user experiences.</p>
+                        </div>
+                    </div>
+                </div>
+                <div className="xl:col-span-1 xl:row-span-2">
+                    <div className="grid-container">
+                        <img src="assets/grid4.png" alt="grid-4" className="w-full md:h-[126px] sm:h-[276px] h-fit object-cover sm:object-top" />
+                        <div className="space-y-2">
+                            <p className="grid-subtext text-center">Contact me</p>
+                            <div className="copy-container" onClick={handleCopy}>
+                                <img src={hasCoped ? "assets/tick.svg" : "assets/copy.svg"} alt="copy" />
+
+                                <p className="lg:text-2xl md:text-xl font-medium text-gray_gradient text-white">nivindulakshitha@gmial.com</p>
+                                
+                            </div>
                         </div>
                     </div>
                 </div>
