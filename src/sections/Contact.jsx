f2f730d3045ae8e7914f8d3dbaf7bea0b2578f37
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-05 13:06:42 +0530

    Refactor Contact section in App component

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -3,10 +3,17 @@ const Contact = () => {
         <section className="c-space my-20">
             <div className="relative min-h-screen flex items-center justify-center flex-col">
                 <img src="/assets/terminal.png" alt="terminal background" className="absolute inset-0 min-h-screen" />
+
+                <div className="contact-container">
+                    <h3 className="head-text">
+                        Let&apos;s talk
+                    </h3>
+                    <p className="text-lg text-white-600 mt-3">
+                        I am always open to discussing new projects, creative ideas or new opportunities to be part of. Feel free to connect with me. 
+                    </p>
+
+                </div>
             </div>
-            <h3 className="head-text">
-                Contact Me
-            </h3>
         </section>
     )
 }
