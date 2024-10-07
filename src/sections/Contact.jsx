2336421265e6bee9920e064908ba9e11c9253c4f
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-07 17:49:22 +0530

    Refactor Contact section in App component to add form functionality

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -1,4 +1,29 @@
+import { useRef, useState } from "react";
+
 const Contact = () => {
+    const formRef = useRef();
+    const [loading, setLoading] = useState(false)
+    const [form, setForm] = useState({
+        name: '',
+        email: '',
+        message: '',
+    })
+
+    const handleInput = ({ target: { name, value } }) => {
+        setForm({...form, [name]: value })
+     }
+
+    
+    // service_3bncnz9
+    const handleSubmit = () => {
+
+        setForm({
+            name: '',
+            email: '',
+            message: '',
+        })
+     }
+
     return (
         <section className="c-space my-20">
             <div className="relative min-h-screen flex items-center justify-center flex-col">
@@ -12,6 +37,26 @@ const Contact = () => {
                         I am always open to discussing new projects, creative ideas or new opportunities to be part of. Feel free to connect with me. 
                     </p>
 
+                    <form ref={formRef} onSubmit={() => { handleSubmit();  setLoading(true)}} className="mt-12 flex flex-col space-y-7">
+                        <label htmlFor="name" className="space-y-3">
+                            <span className="field-label">Full Name</span>
+                            <input type="text" name="name" onChange={handleInput} value={form.name} className="field-input" placeholder="Nivindu Lakshitha" required />
+                        </label>
+                        <label htmlFor="email" className="space-y-3">
+                            <span className="field-label">E-mail Address</span>
+                            <input type="email" name="email" onChange={handleInput} value={form.email} className="field-input" placeholder="nivindulakshtha@gmail.com" required />
+                        </label>
+                        <label htmlFor="message" className="space-y-3">
+                            <span className="field-label">Your Message</span>
+                            <textarea rows={5} name="message" onChange={handleInput} value={form.message} className="field-input" placeholder="Hi, I am John Doe. I am looking forward to working with you. I am open to discussing new projects, creative ideas or new opportunities to be part of. Feel free to connect with me." required />
+                        </label>
+
+                        <button type="submit" className="field-btn" disabled={loading}>
+                            {loading ? "Sending..." : "Send Message"}
+                            
+                            <img src="/assets/arrow-up.png" alt="send" className="field-btn_arrow"/>
+                        </button>
+                    </form>
                 </div>
             </div>
         </section>
