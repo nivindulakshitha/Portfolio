17a12a6392adc5374b5efa91011aa049e1309fe8
nivindulakshitha <nivindulakshitha@gmail.com>
2024-10-07 18:22:07 +0530

    Refactor Contact section in App component to improve form functionality and styling

--- src/sections/Contact.jsx
+++ src/sections/Contact.jsx
@@ -47,18 +47,18 @@ const Contact = () => {
 
     return (
         <section className="c-space my-20">
-            <div className="relative min-h-screen flex items-center justify-center flex-col">
-                <img src="/assets/terminal.png" alt="terminal background" className="absolute inset-0 min-h-screen" />
+            <div className="relative h-screen flex items-center justify-center align-middle flex-row">
+                <img src="/assets/terminal.png" alt="terminal background" className="absolute inset-0 h-screen w-full" />
 
-                <div className="contact-container">
+                <div className="contact-container scale-75">
                     <h3 className="head-text">
                         Let&apos;s talk
                     </h3>
                     <p className="text-lg text-white-600 mt-3">
-                        I am always open to discussing new projects, creative ideas or new opportunities to be part of. Feel free to connect with me. 
+                        I am always open to discussing new projects, new opportunities to be part of.
                     </p>
 
-                    <form ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
+                    <form method="post" ref={formRef} onSubmit={handleSubmit} className="mt-12 flex flex-col space-y-7">
                         <label htmlFor="name" className="space-y-3">
                             <span className="field-label">Full Name</span>
                             <input type="text" name="name" onChange={handleInput} value={form.name} className="field-input" placeholder="Nivindu Lakshitha" required />
@@ -69,7 +69,7 @@ const Contact = () => {
                         </label>
                         <label htmlFor="message" className="space-y-3">
                             <span className="field-label">Your Message</span>
-                            <textarea rows={5} name="message" onChange={handleInput} value={form.message} className="field-input" placeholder="Hi, I am John Doe. I am looking forward to working with you. I am open to discussing new projects, creative ideas or new opportunities to be part of. Feel free to connect with me." required />
+                            <textarea rows={3} name="message" onChange={handleInput} value={form.message} className="field-input" placeholder="I am looking forward to working with you." required />
                         </label>
 
                         <button type="submit" className="field-btn" disabled={loading}>
